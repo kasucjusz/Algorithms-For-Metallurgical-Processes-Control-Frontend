112015402ec0335cf4c65f3ea03d4551e390f039
barto <bartosz.kaszaa@gmail.com>
2020-05-15 14:46:35 +0200

    usunieto niepotrzebne parametry, dodano wyliczanie mediany

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,22 @@
 import {Component, OnInit} from '@angular/core';
 import {ConfigService} from '../httpConnector/config.service';
-import { Chart } from 'chart.js';
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   chart: Chart[] = [];
-
+  median;
+  average;
   dataSource;
   title = 'aspm-frontend';
   displayedColumns = [
     'Czas',
-    'P1010',
-    'P1012',
-    'P1014',
-    'P1034',
-    'P1036',
-    'P1038',
-    'P1044',
-    'P1046',
-    'P1048',
-    'P1050',
-    'P1052',
-    'P1054',
-    'P1058',
-    'P1060',
-    'P1062',
-    'P1064',
-    'P1066',
-    'P1068',
-    'P1070',
-    'P1072',
-    'P1074',
-    'P1076',
-    'P1078',
-    'P1080',
-    'P1094',
-    'P1096',
-    'P1098',
-    'P1100',
-    'P1102',
-    'P1104',
-    'P1106',
     'P1108',
-    'P1110',
-    'P1112',
-    'P1114',
     'P1118',
     'P1120',
     'P1122',
@@ -58,12 +25,6 @@ export class AppComponent implements OnInit{
     'P1128',
     'P1130',
     'P1132',
-    'P1134',
-    'P1136',
-    'P1138',
-    'P1140',
-    'P1142',
-    'P1144',
     'P1146',
     'P1148',
     'P1150',
@@ -77,23 +38,7 @@ export class AppComponent implements OnInit{
     'P1166',
     'P1168',
     'P1170',
-    'P1172',
-    'P1174',
-    'P1176',
-    'P1178',
-    'P1180',
-    'P1182',
-    'P1184',
-    'P1186',
-    'P1188',
-    'P1192',
-    'P1194',
-    'P1426',
-    'P1427',
-    'P1433',
-    'P1434',
-    'P1435',
-    'P1440'
+    'P1172'
   ];
 
   constructor(private httpConnector: ConfigService) {
@@ -124,7 +69,7 @@ export class AppComponent implements OnInit{
   changeSpeed($event) {
     this.chart = [];
     const param = $event.source.value;
-    if($event.isUserInput) {
+    if ($event.isUserInput) {
       console.log(param);
       if (param === 'all') {
         this.httpConnector.getAllData()
@@ -155,10 +100,10 @@ export class AppComponent implements OnInit{
     const json = JSON.stringify(this.dataSource);
     JSON.parse(json, (key, value) => {
       if (key.toLowerCase() === param.toLowerCase()) {
-        console.log(value);
+        console.log('WARTOSC', value);
         values.push(value);
       }
-      if(key.toLowerCase() === 'time') {
+      if (key.toLowerCase() === 'time') {
         dates.push(value);
       }
     });
@@ -190,7 +135,20 @@ export class AppComponent implements OnInit{
         }
       }
     });
+    console.log('WARTOSCI ', values);
+    console.log('MEDIANA',  median(values));
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+  }
+}
+
+function median(values) {
+  values.sort( function(a,b) {return a - b;} );
+  var half = Math.floor(values.length/2);
+
+  if(values.length % 2)
+    return values[half];
+  else
+    return (Number(values[half])+ Number(values[half+1]))/2;
 }
